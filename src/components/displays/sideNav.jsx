93c7a3e754fab2214423ad9fd8ad68a95ef78ee7
ebsamson3 <ebsamson3@gmail.com>
2019-12-31 17:22:23 -0600

    Added navbar styling to match census toolkit

--- src/components/displays/sideNav.jsx
+++ src/components/displays/sideNav.jsx
@@ -3,12 +3,20 @@ import React from 'react';
 export default class NavBar extends React.Component {
   render() {
     return (
-      <li 
-        className={`hover ${this.props.isCurrent ? 'navbar-in-view' : 'navbar-not-in-view'}`}
+      <div 
+        className='hover navbar-item'
         onClick={ this.props.onClick }
-      >
-        {this.props.name}
-      </li>
+       >
+        <div 
+          className={`navbar-circle ${this.props.isCurrent ? 'navbar-circle-selected' : ''}`}
+        >
+        </div>
+        <li 
+          className={`${this.props.isCurrent ? 'navbar-in-view' : 'navbar-not-in-view'}`}
+        >
+          {this.props.name}
+        </li>
+      </div>
     );
   }
 }
