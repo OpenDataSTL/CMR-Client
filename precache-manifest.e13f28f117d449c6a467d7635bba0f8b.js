e203849ad16747eee847b9d9fd6cd596845b498d
pubby <pubby8@gmail.com>
2020-01-28 10:43:31 -0600

    Updates

---
+++ precache-manifest.e13f28f117d449c6a467d7635bba0f8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "892153b7ddb1501f2b7f6a797810dec6",
+    "url": "/CMR-Client/index.html"
+  },
+  {
+    "revision": "e4600e2b966ab64f462e",
+    "url": "/CMR-Client/static/css/main.be7dba64.chunk.css"
+  },
+  {
+    "revision": "f997c7ec757b5be70d21",
+    "url": "/CMR-Client/static/js/2.897a8319.chunk.js"
+  },
+  {
+    "revision": "6adce59638ff1f12aa03e688a4fb1552",
+    "url": "/CMR-Client/static/js/2.897a8319.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4600e2b966ab64f462e",
+    "url": "/CMR-Client/static/js/main.e4f728f2.chunk.js"
+  },
+  {
+    "revision": "e8692b71dfbd60a0f9e0",
+    "url": "/CMR-Client/static/js/runtime-main.607a259c.js"
+  }
+]);
